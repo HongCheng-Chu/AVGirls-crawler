8e4a2e4ee3da2a6ad4b4028c3d99eab445d36c49
HongCheng-Chu <jerry840622@gmail.com>
2021-12-05 22:16:08 +0800

    Add files via upload

---
+++ Girls_crawler.py
@@ -0,0 +1,233 @@
+import requests
+import os
+import time
+import json
+import random
+import re
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+
+def get_html(url):
+    response = requests.get(url, headers = get_headers(), allow_redirects=True)
+    return response.text # text return Unicode data -> get text
+
+
+def get_content(url):
+    response = requests.get(url, headers = get_headers(), timeout = 10)
+    return response.content # content return bytes(binary) data -> get image, video, file and etc
+
+
+def download_obj(data, path):
+    with open(path, "wb") as file:
+        file.write(data)
+        file.close()
+
+
+def get_headers():
+    ua = UserAgent()
+    headers = {'user-agent': ua.random, 'cookie': cookie}
+    return headers
+
+
+def search(actresses):
+
+    ChromeOptions = Options()
+
+    ChromeOptions.add_argument('--headless')
+
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    ChromeOptions.add_experimental_option("prefs", prefs)
+
+    driver = webdriver.Chrome(chrome_options = ChromeOptions)
+
+    time.sleep(5)
+
+    driver.get("https://s1s1s1.com/top")
+
+    time.sleep(5)
+
+    actresses_url = []
+
+    for actress in actresses:
+
+        driver.find_element_by_name("keyword").send_keys(actress, Keys.ENTER)
+
+        actresses_url.append(driver.current_url)
+
+        time.sleep(5)
+
+    cookie = [item["name"] + "=" + item["value"] for item in driver.get_cookies()]
+    cookiestr = ";".join(item for item in cookie)
+
+    return actresses_url, cookiestr
+
+
+def get_post(girl_url, next_page):
+
+    post_links = []
+
+    image_links = []
+
+    if next_page == 'none':
+
+        page_html = get_html(girl_url)
+
+        page_soup = BeautifulSoup(page_html, 'html.parser')
+
+        cards = page_soup.find_all("div", {'class':"c-card"})
+
+        for card in cards:
+
+            post_links.append(card.find("a", {'class':"img hover"})["href"])
+
+            image = card.find("img")["data-src"]
+
+            image_links.append(image)
+            
+            time.sleep(5)
+
+    while not next_page == 'none':
+
+        page_html = get_html(girl_url)
+
+        page_soup = BeautifulSoup(page_html, 'html.parser')
+
+        cards = page_soup.find_all("div", {'class':"c-card"})
+
+        for card in cards:
+
+            post_links.append(card.find("a", {'class':"img hover"})["href"])
+
+            image = card.find("img")["data-src"]
+
+            image_links.append(image)
+            
+            time.sleep(5)
+
+        try:
+            next_page = page_soup.find("a", {"rel": "next"})["href"]
+
+        except:
+            next_page = 'none'
+
+        girl_url = next_page
+
+        time.sleep(5)
+
+    return post_links, image_links
+
+
+def get_issue_data(girl_cards, name):
+
+    video_issue = []
+
+    for card in girl_cards:
+
+        page_html = get_html(card)
+
+        page_soup = BeautifulSoup(page_html, 'html.parser')
+
+        datas = page_soup.find_all("div", {"class": "td"})
+
+        day = datas[1].find("a", {"class": "c-tag c-main-bg-hover c-main-font c-main-bd"})["href"];
+
+        issue_day = day.split('/')[-1]
+
+        issue_number = card.split('/')[-1]
+
+        issue_title = page_soup.find("h2", {"class": "p-workPage__title"}).getText()
+
+        video_data = (issue_day.strip()) + " " + (issue_number.strip()) + " " + name + " "+ (issue_title.strip())
+
+        video_issue.append(video_data)
+
+        time.sleep(5)
+    
+    return video_issue
+
+
+def Download_post(images, video_issue, name):
+
+    dirpath = r'.\Girls_video.\{0}'.format(name)
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+    
+    for i, j in zip(images, video_issue):
+
+        image = get_content(i)
+
+        file_type = i.split('.')[-1]
+
+        file_path = r'.\girls_video.\{0}\{1}.{2}'.format(name, j, file_type)
+
+        try:
+            download_obj(image, file_path)
+
+            print('{0} download success'.format(j))
+
+        except:
+            print('{0} download fail'.format(j))
+       
+        
+def main(girls_url, girl_name):
+
+    iter = 0
+
+    for girl_url in girls_url:
+
+        html = get_html(girl_url)
+
+        soup = BeautifulSoup(html, "html.parser")
+
+        try :
+            next_page = soup.find("a", {"rel": "next"})["href"]
+        except:
+            next_page = 'none'
+    
+        print('get next page success')
+
+        posts, images = get_post(girl_url, next_page)
+
+        print('get post success')
+
+        video_issue = get_issue_data(posts, girl_name[iter])
+
+        print('get issue data success')
+    
+        Download_post(images, video_issue, girl_name[iter])
+
+        print('Download all post image & video success')
+
+        iter = iter + 1
+
+
+if __name__ == '__main__':
+
+    start = time.time()
+    
+    actresses = []
+
+    with open('AVgirls.txt', 'r', encoding='utf8') as names:
+
+        for name in names:
+
+            actresses.append(name.rstrip('\n'))
+
+    url, cookie = search(actresses)
+
+    main(url, actresses)
+
+    print(' Success !!!! ╮(╯  _ ╰ )╭')
+
+    end = time.time()
+
+    total_time = end - start
+    hour = total_time // 3600
+    min = (total_time - 3600 * hour) // 60
+    sec = total_time - 3600 * hour - 60 * min
+    print(f'Totel spend time:{int(hour)}h {int(min)}m {int(sec)}s')
+
