cb475fae58cc41cf60bd9bd16513e2b8a7338c74
HongCheng-Chu <jerry840622@gmail.com>
2021-12-20 01:04:31 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import save_files
+import json
+from S1 import s1_crawler
+from S1 import s1_updater
+import madonna_crawler
+import faleno_crawler
+import faleno_updater
+
+if __name__ == '__main__':
+    #s1_crawler.main()
+    faleno_updater.main()
